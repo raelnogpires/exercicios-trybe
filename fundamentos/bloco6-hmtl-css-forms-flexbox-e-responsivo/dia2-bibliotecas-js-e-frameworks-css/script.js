e649e98597c32c454ea123e2e35ba0159160d801
Rael N. Pires <rnogpiresfinancial@gmail.com>
2021-09-20 11:03:07 -0300

    Exercicio finalizado com ajuda do gabarito

--- fundamentos/bloco6-hmtl-css-forms-flexbox-e-responsivo/dia2-bibliotecas-js-e-frameworks-css/script.js
+++ fundamentos/bloco6-hmtl-css-forms-flexbox-e-responsivo/dia2-bibliotecas-js-e-frameworks-css/script.js
@@ -1,4 +1,91 @@
-// script.js
+import './node_modules/just-validate/dist/js/just-validate.js';
+
+// validação de formulário usando a biblioteca Just Validate
+new window.JustValidate('.form', {
+  rules: {
+    name: {
+      required: true,
+      minLength: 3,
+      maxLength: 40
+    },
+    email: {
+      required: true,
+      maxLength: 50,
+      minLength: 5
+    },
+    cpf: {
+      required: true,
+      maxLength: 11
+    },
+    adress: {
+      required: true,
+      maxLength: 200
+    },
+    city: {
+      required: true,
+      maxLength: 28
+    },
+    state: {
+      required: true
+    },
+    housetype: {
+      required: true
+    },
+    resume: {
+      required: true
+    },
+    role: {
+      required: true
+    },
+    roleDescription: {
+      required: true
+    },
+    startDate: {
+      required: true,
+      maxLength: 10,
+      minLength: 1
+    },
+    submitHandler: function (form, values) {
+      console.log(form, values);
+    }
+  }
+});
+
+// 'validação' de data usando a biblioteca Pikaday
+const picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
+});
+
+const clearButton = document.querySelector('.clear-button');
+
+// limpar campos preenchidos
+function clearFields() {
+  const formElements = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea')
+  for (let index = 0; index < formElements.length; index += 1) {
+    const userInput = formElements[index];
+    userInput.value = '';
+    textArea.value = '';
+  }
+}
+
+clearButton.addEventListener('click', clearFields);
+
+// criação das opções de estado
 function createStateOptions() {
   let states = document.getElementById('state');
   let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
@@ -10,298 +97,6 @@ function createStateOptions() {
   }
 }
 
-let inputs = {
-  name: {
-    maxLength: 40,
-    required: true,
-  },
-  email: {
-    maxLength: 50,
-    required: true
-  },
-  cpf: {
-    maxLength: 11,
-    required: true
-  },
-  address: {
-    maxLength: 200,
-    required: true
-  },
-  city: {
-    maxLength: 28,
-    required: true,
-  },
-  state: {
-    type: 'select',
-    required: true,
-  },
-  houseType: {
-    type: 'radio',
-    required: true,
-  },
-  resume: {
-    maxLength: 1000,
-    required: true,
-  },
-  role: {
-    maxLength: 40,
-    required: true,
-  },
-  roleDescription: {
-    maxLength: 500,
-    required: true,
-  },
-  startDate: {
-    type: 'date',
-    required: true,
-  }
-}
-
-function defaultValidation(input, name){
-  let trimmed = input.value.trim();
-  let validation = inputs[name];
-
-  if(validation.required && trimmed.length === 0){
-    return false;
-  }
-
-  if(validation.maxLength && trimmed.length > validation.maxLength){
-    return false;
-  }
-  
-  return true;
-}
-
-function dateValidation(input, name){
-  if(input.value.length === 0){
-    return {
-      message: 'A data não foi preenchida!'
-    }
-  }
-
-  let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
-  
-  if(!regex.test(input.value)){
-    return {
-      message: 'Data: Formato inválido'
-    };
-  }
-
-  let splitted = input.value.split('/');
-  let day = splitted[0];
-  let month = splitted[1];
-  let year = splitted[2];
-
-  if(day < 0 || day > 30){
-    return {
-      message: 'Dia inválido'
-    };
-  }
-
-  if(month < 0 || month > 12){
-    return {
-      message: 'Mês inválido'
-    }
-  }
-
-  if(year < 0) {
-    return {
-      message: 'Ano inválido'
-    };
-  }
-
-  return true;
-}
-
-function getSelectedOption(select){
-  return select.options[select.selectedIndex];
-}
-
-function selectValidation(select, name){
-  let option = getSelectedOption(select);
-  let validation = inputs[name];
-
-  if(validation.required && (!option || option.disabled)){
-    return false;
-  }
-
-  return true;
-}
-
-function radioValidation(radio, name){
-  let checked = document.querySelector(`[name=${name}]:checked`)
-  
-  if(checked === null){
-    return false;
-  }
-
-  return true;
-}
-
-let validationStrategies = {
-  default: defaultValidation,
-  date: dateValidation,
-  select: selectValidation,
-  radio: radioValidation,
-}
-
-function validateInput(inputName){
-  let inputType = inputs[inputName].type;
-  let input = document.querySelector(`[name=${inputName}]`)
-
-  if(inputType){
-    let validationFunction = validationStrategies[inputType];
-    return validationFunction(input, inputName);
-  }
-
-  return validationStrategies.default(input, inputName);
-}
-  
-function renderErrorMessages(messages){
-  let form = document.querySelector('form');
-  let messageDiv = document.createElement('div');
-  messageDiv.className = 'error';
-  form.prepend(messageDiv);
-
-  for(let message of messages){
-    let p = document.createElement('p');
-    p.innerText = message;
-
-    messageDiv.appendChild(p);
-  }
-}
-
-function defaultRendering(input){
-  let p = document.createElement('p');
-  p.innerText = input.value;
-
-  return p;
-}
-
-function radioRendering(input){
-  let p = document.createElement('p');
-  let name = input.getAttribute('name');
-  let checked = document.querySelector(`[name=${name}]:checked`);
-
-  if(checked){
-    p.innerText = checked.value;
-  }
-
-  return p;
-}
-
-function selectRendering(input){
-  let p = document.createElement('p');
-  let option = getSelectedOption(input)
-  p.innerText = option.value;
-  
-  return p;
-}
-
-let renderStrategies = {
-  default: defaultRendering,
-  radio: radioRendering,
-  select: selectRendering,
-}
-
-function renderData(){
-  let dataDiv = document.createElement('div');
-  dataDiv.className = 'data';
-
-  let form = document.querySelector('form');
-  form.prepend(dataDiv);
-
-  for(let name in inputs){
-    let inputType = inputs[name].type;
-    let input = document.querySelector(`[name=${name}]`);
-
-    let element;
-
-    if(renderStrategies[inputType]){
-      element = renderStrategies[inputType](input, dataDiv)
-    } else {
-      element = renderStrategies.default(input, dataDiv)
-    }
-
-    dataDiv.appendChild(element);
-  }
-}
-
-function validateData(){
-  let validationsList = {};
-
-  for(let inputName in inputs){
-    let isValid = validateInput(inputName);
-    validationsList[inputName] = isValid;
-  }
-  
-  let counter = 0;
-  let messages = [];
-
-  for(let index in validationsList){
-    if(validationsList[index] === false){
-      counter += 1;
-    }
-
-    if(validationsList[index].message){
-      counter += 1;
-      messages.push(validationsList[index].message);
-    }
-  }
-
-  return {
-    errorQtd: counter,
-    messages,
-  }
-}
-
-function clearDivs(){
-  let errorDivs = document.querySelectorAll('.error');
-
-  for(div of errorDivs){
-    div.remove();
-  }
-
-  let dataDiv = document.querySelector('.data');
-
-  if(dataDiv){
-    dataDiv.remove();
-  }
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  let validation = validateData();
-
-  clearDivs();
-  
-  if(validation.errorQtd === 0){
-    renderData();
-  } else {
-    validation.messages.unshift('Dados Inválidos')
-
-    renderErrorMessages(validation.messages)
-  }
-}
-
-function clearFields() {
-  let formElements = document.querySelectorAll('input');
-  let textArea = document.querySelector('textarea')
-  let div = document.querySelectorAll('.div-curriculum');
-  for (let index = 0; index < formElements.length && index < div.length; index += 1) {
-    let userInput = formElements[index];
-    userInput.value = '';
-    textArea.value = '';
-    div[index].innerText = '';
-  }
-}
-
-window.onload = function () {
+window.onload = function initialize() {
   createStateOptions();
-  let submitButton = document.querySelector('.submit-button');
-  submitButton.addEventListener('click', handleSubmit);
-
-  let clearButton = document.querySelector('.clear-button');
-  clearButton.addEventListener('click', clearFields)
 }
