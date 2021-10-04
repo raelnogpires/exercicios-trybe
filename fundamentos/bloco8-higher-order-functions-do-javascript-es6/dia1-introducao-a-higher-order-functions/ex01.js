e2aee6504fcbc0df48ab296c4d450029b5de26c7
Rael N. Pires <rnogpiresfinancial@gmail.com>
2021-10-04 15:51:41 -0300

    Exercicio 1 finalizado

---
+++ fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia1-introducao-a-higher-order-functions/ex01.js
@@ -0,0 +1,9 @@
+const wakeUp = () => 'Acordando!!';
+
+const haveCoffee = () => 'Bora tomar café!!';
+
+const japasParty = () => 'Partiu dormir!!';
+
+const doingThings = (callback) => callback;
+
+console.log(doingThings(japasParty()));
