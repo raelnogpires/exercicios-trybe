188aa5f452081d8ae0dcc6865463133a68937b56
Rael N. Pires <rnogpiresfinancial@gmail.com>
2021-11-04 09:25:18 -0300

    second part of fixation exercises completed

--- front-end/bloco11-componentes-com-estado-eventos-e-formularios-no-react/dia1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco11-componentes-com-estado-eventos-e-formularios-no-react/dia1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,24 +1,9 @@
 import './App.css'
-
-const handleFirstButton = () => {
-  console.log('Funcionou! XD');
-};
-
-const handleSecondButton = () => {
-  console.log('Esse aqui também está funcionando :O');
-};
-
-const handleThirdButton = () => {
-  console.log('O último funcionou também, ufa! :P');
-};
+import Buttons from './Buttons';
 
 function App() {
   return (
-    <div className='button-div'>
-      <button onClick={handleFirstButton}>Botão Nº1</button>
-      <button onClick={handleSecondButton}>Botão Nº2</button>
-      <button onClick={handleThirdButton}>Botão Nº3</button>
-    </div>
+    <Buttons />
   );
 }
 
