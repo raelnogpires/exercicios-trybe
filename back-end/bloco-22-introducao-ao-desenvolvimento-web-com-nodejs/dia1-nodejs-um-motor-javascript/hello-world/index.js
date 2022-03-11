a7ab5284b95c4516c3d05570d72cd7a6b090bcb3
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-11 13:22:05 -0300

    hello-world package improved with readline-sync

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia1-nodejs-um-motor-javascript/hello-world/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia1-nodejs-um-motor-javascript/hello-world/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, World');
+const { question, questionInt } = require('readline-sync');
+// console.log('Hello, World');
+
+const name = question('Qual é o seu nome? ');
+const age = questionInt('Quantos anos você tem? ');
+
+console.log(`Olá, ${name}! Você tem ${age} anos.`);
