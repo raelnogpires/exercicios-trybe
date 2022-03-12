6f102302e7428e76e73ab6abfd92d0ca57fe93c7
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-11 21:37:13 -0300

    npm start gives the user a choice to what package to use

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia1-nodejs-um-motor-javascript/my-scripts-exercise/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia1-nodejs-um-motor-javascript/my-scripts-exercise/index.js
@@ -1,3 +1,21 @@
-// const { returnUserIMC } = require('./imc');
+const { questionInt } = require('readline-sync');
 
-// returnUserIMC();
+const scripts = [
+  { name: 'IMC Calculator', script: './imc.js' },
+  { name: 'Average Speed Calculator', script: './velocidade.js' },
+  { name: 'Guess What Number Game', script: './sorteio.js' }
+];
+
+let numberedScripts = scripts.map((s, i) => `${i + 1} - ${s.name}`);
+
+numberedScripts.unshift('Escolha um número para executar seu script: ');
+
+numberedScripts = numberedScripts.join('\n');
+
+const scriptNumber = questionInt(numberedScripts) - 1;
+
+const runningScript = scripts[scriptNumber];
+
+if (!scriptNumber) console.log('Número inválido.');
+
+require(runningScript.script);
