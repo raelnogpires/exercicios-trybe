f625f309fea85e883c1643e034ee89cd15e71690
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-23 19:08:31 -0300

    padronization in Author.js

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
@@ -1,11 +1,26 @@
 const connection = require('./connection');
 
+const camelCase = (data) => ({
+  id: data.id,
+  firstName: data.first_name,
+  middleName: data.middle_name,
+  lastName: data.last_name,
+});
+
+const fullName = ({ id, firstName, middleName, lastName }) => {
+  const fullName = [firstName, middleName, lastName]
+    .filter(Boolean)
+    .join(' ');
+
+  return { id, firstName, middleName, lastName, fullName };
+};
+
 const getAll = async () => {
   const [authors] = await connection.execute(
     'SELECT id, first_name, middle_name, last_name FROM model_example.authors;'
   );
 
-  return authors;
+  return authors.map(camelCase).map(fullName);
 };
 
 module.exports = {
