ed3821a9d7fabc4327cf96c2772a44c17a52c3bf
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-28 18:54:14 -0300

    feat: productController.js all refactored, routers/products.js implemented

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia3-arquitetura-web-rest-e-restful/controllers/productController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia3-arquitetura-web-rest-e-restful/controllers/productController.js
@@ -1,47 +1,119 @@
-const express = require('express');
 const ProductModel = require('../models/productModel');
-
-const router = express.Router();
 
 const HTTP_OK_STATUS = 200;
 const INTERNAL_ERROR = 500;
+const CREATED_STATUS = 201;
 
-router.get('/products', async (_req, res) => {
+// router.get('/products', async (_req, res) => {
+//   try {
+//     const products = await ProductModel.getAll();
+//     return res.status(HTTP_OK_STATUS).json(products);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+//   }
+// });
+
+const getAll = async (_req, res) => {
   try {
     const products = await ProductModel.getAll();
-    res.status(HTTP_OK_STATUS).json(products);
+    return res.status(HTTP_OK_STATUS).json(products);
   } catch (error) {
     console.log(error);
-    res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+    return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
   }
-});
+};
 
-router.get('/products/:id', async (req, res, next) => {
-  const product = await ProductModel.getById(req.params.id);
+// router.get('/products/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await ProductModel.getById(id);
+//     return res.status(HTTP_OK_STATUS).json(product);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+//   }
+// });
 
-  res.send(product);
-});
+const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await ProductModel.getById(id);
+    return res.status(HTTP_OK_STATUS).json(product);
+  } catch (error) {
+    console.log(error);
+    return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+  }
+};
 
-router.post('/user', async (req, res) => {
-  const { name, brand } = req.body;
+// router.post('/user', async (req, res) => {
+//   try {
+//     const newProduct = await ProductModel.add(req.body);
+//     res.status(CREATED_STATUS).json(newProduct);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+//   }
+// });
 
-  const newProduct = await ProductModel.add(name, brand);
+const addNewProduct = async (req, res) => {
+  try {
+    const newProduct = await ProductModel.add(req.body);
+    res.status(CREATED_STATUS).json(newProduct);
+  } catch (error) {
+    console.log(error);
+    return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+  }
+};
 
-  res.send(newProduct);
-});
+// router.delete('/user/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     await ProductModel.exclude(id);
+//     res.status(200).end();
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+//   }
+// });
 
-router.delete('/user/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+const deleteById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await ProductModel.exclude(id);
+    res.status(200).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+  }
+};
 
-  res.send(products);
-});
+// router.put('/user/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const products = await ProductModel.update(id, ...req.body);
+//     res.status(200).json(products);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+//   }
+// });
 
-router.put('/user/:id', async (req, res) => {
-  const { name, brand } = req.body;
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await ProductModel.update(id, ...req.body);
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    return res.status(INTERNAL_ERROR).json({ message: 'Ops, alguma coisa deu errado! :(' });
+  }
+};
 
-  const products = await ProductModel.update(req.params.id, name, brand);
-
-  res.send(products);
-});
-
-module.exports = router;
+module.exports = {
+  getAll,
+  getProductById,
+  addNewProduct,
+  deleteById,
+  update
+};
