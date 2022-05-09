ef4cb7ea44a2b1c5f70624bb61260d33994828ed
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-09 13:35:20 -0300

    feat: now compiled

--- back-end/bloco-26-typescript/dia1-introducao/index.ts
+++ back-end/bloco-26-typescript/dia1-introducao/index.ts
@@ -15,3 +15,20 @@ console.log(`Quadrado de lado 13cm: ${f.square(13)}cm²`);
 
 console.log(`Retângulo de base 10cm e altura 45cm: ${f.rectangle(10, 45)}cm²`);
 console.log(`Retângulo de base 97cm e altura 205cm: ${f.rectangle(100, 200)}cm²`);
+
+// Retorno:
+// Olá, João! Como você está?
+
+// Olá Rael, você tem 21 anos!
+// Olá Lara, você tem 21 anos. Espere, hoje é seu aniversário? Parabéns! :)
+
+// A soma do array é igual a: 24
+
+// Triângulo de base 10cm e e altura 17cm: 85cm²
+// Triângulo de base 8cm e e altura 13cm: 52cm²
+
+// Quadrado de lado 8cm: 64cm²
+// Quadrado de lado 13cm: 169cm²
+
+// Retângulo de base 10cm e altura 45cm: 450cm²
+// Retângulo de base 97cm e altura 205cm: 20000cm²
